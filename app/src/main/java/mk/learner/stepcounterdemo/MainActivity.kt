9ce2670ec7f4467dffd405a4e9ee369494ecba3c
Mg Kaung <mymemory692018@gmail.com>
2020-09-10 21:04:55 +0630

    foreground service

--- app/src/main/java/mk/learner/stepcounterdemo/MainActivity.kt
+++ app/src/main/java/mk/learner/stepcounterdemo/MainActivity.kt
@@ -1,19 +1,20 @@
 package mk.learner.stepcounterdemo
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() , SensorEventListener {
 
-    var running = false
-    lateinit  var sensorManager: SensorManager
+    private var running = false
+    private lateinit  var sensorManager: SensorManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,29 +22,35 @@ class MainActivity : AppCompatActivity() , SensorEventListener {
 
         //init sensor Manager
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        btn_next.setOnClickListener{
+            startActivity(Intent(this,SecondActivity::class.java))
+        }
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onSensorChanged(event: SensorEvent) {
         if (running)  stepsValue.text= "Today Step ${event.values[0]}"
     }
 
-    override fun onAccuracyChanged(event: Sensor?, p1: Int) {
-
-    }
+    override fun onAccuracyChanged(event: Sensor?, p1: Int) {}
 
     override fun onResume() {
         super.onResume()
         running = true
-        var stepsSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        val stepsSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
         if (stepsSensor == null) {
-            Toast.makeText(this, "No Step Counter Sensor !", Toast.LENGTH_SHORT).show()
+            toast("No Step Counter Sensor !")
         } else {
-            sensorManager?.registerListener(this, stepsSensor, SensorManager.SENSOR_DELAY_UI)
+            sensorManager.registerListener(this, stepsSensor, SensorManager.SENSOR_DELAY_UI)
         }
     }
+
+
+
     override fun onPause() {
         super.onPause()
         running = false
-        sensorManager?.unregisterListener(this)
+        sensorManager.unregisterListener(this)
     }
 }
