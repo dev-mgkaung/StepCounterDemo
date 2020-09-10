2668265f8d13c2bc40e9878dfb3b333fbe5207c6
Mg Kaung <mymemory692018@gmail.com>
2020-09-10 18:05:13 +0630

    Initial commit

---
+++ app/src/main/java/mk/learner/stepcounterdemo/MainActivity.kt
@@ -0,0 +1,49 @@
+package mk.learner.stepcounterdemo
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() , SensorEventListener {
+
+    var running = false
+    lateinit  var sensorManager: SensorManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //init sensor Manager
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (running)  stepsValue.text= "Today Step ${event.values[0]}"
+    }
+
+    override fun onAccuracyChanged(event: Sensor?, p1: Int) {
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        running = true
+        var stepsSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        if (stepsSensor == null) {
+            Toast.makeText(this, "No Step Counter Sensor !", Toast.LENGTH_SHORT).show()
+        } else {
+            sensorManager?.registerListener(this, stepsSensor, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+    override fun onPause() {
+        super.onPause()
+        running = false
+        sensorManager?.unregisterListener(this)
+    }
+}
